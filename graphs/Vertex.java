085416be9a52398d8aa23077df3916a5a5937930
abarax <leigh.appel@gmail.com>
2013-08-05 09:01:36 +1000

    Basic vertex

--- graphs/Vertex.java
+++ graphs/Vertex.java
@@ -2,13 +2,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
-/**
- * Created with IntelliJ IDEA.
- * User: abarax
- * Date: 4/08/13
- * Time: 8:27 PM
- * To change this template use File | Settings | File Templates.
- */
 public class Vertex
 {
     public String name;
@@ -19,15 +12,9 @@ public class Vertex
         this.name = name;
     }
 
-    /// <summary>
-    /// Create a new Edge, connecting this Vertex to the Nod passed in the parameter
-    /// Also, it creates the inversed Vertex in the passed Vertex
-    /// </summary>
     public Vertex addEdge(Vertex child, int w)
     {
         edges.add(new Edge(this, child, w));
-
-        child.addEdge(this, w);
 
         return this;
     }
